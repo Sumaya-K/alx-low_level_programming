797fb5bae687a7db118a05874aa652622b3178ea
Sumaya-K <somakmbal@gmail.com>
2022-12-11 10:34:10 -0800

    print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *this script to prniy the followed sentecnce
+ */
+char main()
+{
+	char print[] = "\"Programming is like building a multilingual puzzle";
+		puts(print);
+
+	return (0);
+}
