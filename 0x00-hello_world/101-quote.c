cbb52d00f2db4cb462b2c442d96d5cb111ddb6ef
Sumaya-K <somakmbal@gmail.com>
2022-12-12 15:56:08 -0800

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, %d-%d-%d\n", "2015", "10", "19")
+	return (1)
+}
