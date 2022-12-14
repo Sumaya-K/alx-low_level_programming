264862d74fe275d32dc97c30af539cb49eeed7b8
Sumaya-K <somakmbal@gmail.com>
2022-12-14 06:34:14 -0800

    letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
