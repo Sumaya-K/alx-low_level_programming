8a39f10b7983ea684fff4b6af1c9e2deec3d53c4
Sumaya-K <somakmbal@gmail.com>
2022-12-16 15:56:13 -0800

    new repo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - this function test if c is uppercase
+ * @c: cherachter to check
+ *
+ * Return: Always 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This repo is for trying more while loops and
+functions in C
